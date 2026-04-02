0c5c445c19ff9f150efafcd0692af53c55b706b3
Vinay <vinayydv2003@gmail.com>
2026-04-02 11:48:38 +0530

    server url change

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
 export const authDataContext=createContext()
 function AuthContext({children}) {
-const serverUrl="http://localhost:8000"
+const serverUrl="https://kindfind-backend.onrender.com"
     let value={
         serverUrl
     }
